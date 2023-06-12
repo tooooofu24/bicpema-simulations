2bf0b227f4506ed4e6b024d32fcbf529344fd15c
ohiaeni <jitianxianer018@gmail.com>
2023-06-12 16:56:40 +0900

    斜方投射シミュレーションの更新

--- public/simulations/projectile-motion2/js/index.js
+++ public/simulations/projectile-motion2/js/index.js
@@ -1,7 +1,9 @@
+// 全体表示のための関数
 function fullScreen() {
-    createCanvas(4 * windowWidth / 5, windowHeight)
+    createCanvas(windowWidth, 9 * windowHeight / 10)
 }
 
+// 画像の読み込み
 let redBallImg,
     blueBallImg,
     yellowBallImg;
@@ -11,24 +13,40 @@ function preload() {
     yellowBallImg = loadImage("../../assets/img/yellowBallImg.png")
 }
 
+// DOM要素の生成
+let elementX,
+    elementY;
+let parentEl;
 let speedExpla,
-    speedSlider;
+    speedInput;
 let thetaExpla,
-    thetaSlider;
-function buttonCreation() {
+    thetaInput;
+let scaleExpla,
+    scaleInput;
+function elCreate() {
+    parentDiv = createDiv("").addClass("parentDiv")
     speedExpla = createElement('label', "速度[m/s]")
-    speedSlider = createInput(18, "number", 1)
+    speedInput = createInput(20, "number", 1)
     thetaExpla = createElement('label', "角度[°]")
-    thetaSlider = createInput(30, "number", 1)
+    thetaInput = createInput(30, "number", 1)
+    scaleExpla = createElement('label', "スケール[倍]")
+    scaleInput = createInput(1, "number", 1)
 }
 
-function buttonSettings() {
-    speedExpla.size(windowWidth / 5, height / 10).position(width, 0).addClass('h1').style('text-align','center').style('line-height',height/10+'px')
-    speedSlider.size(windowWidth / 5, height / 10).position(width, height / 10).input(initValue).addClass('h1').style('text-align','center')
-    thetaExpla.size(windowWidth / 5, height / 10).position(width, 2 * height / 10).addClass('h1').style('text-align','center').style('line-height',height/10+'px')
-    thetaSlider.size(windowWidth / 5, height / 10).position(width, 3 * height / 10).input(initValue).addClass('h1').style('text-align','center')
+// DOM要素の初期設定
+function elInit() {
+    elementX = 4 * width / 5
+    elementY = windowHeight / 10
+    parentDiv.position(elementX, elementY).style("background-color:rgba(255, 255, 255, 0.538); width:20vw; height:35vh;")
+    elArr = [speedExpla, speedInput, thetaExpla, thetaInput, scaleExpla, scaleInput]
+    for (let i = 0; i < elArr.length; i++) {
+        elArr[i].parent(parentDiv).style("width: 80%;height: 5vh;text-align: center;line-height: 5vh;display: block;margin: 0 auto;")
+        if (i % 2 == 1) elArr[i].input(initValue).attribute("min", 0)
+        else elArr[i].style("font-weight: bold;font-size: large;")
+    }
 }
 
+// 初期値の設定
 let redBall,
     blueBall,
     yellowBall;
@@ -38,9 +56,9 @@ let ballRadi,
 let gravity = 9.8,
     time;
 function initValue() {
-    ballRadi = 25
-    ballSpeed = speedSlider.value()
-    ballTheta = thetaSlider.value()
+    ballRadi = width / 50
+    ballSpeed = speedInput.value()
+    ballTheta = thetaInput.value()
     redBallImg.resize(ballRadi * 2, ballRadi * 2)
     redBall = new Ball(ballRadi, height - ballRadi * 2, ballSpeed, ballSpeed, radians(ballTheta), redBallImg)
     redBall.speedx0 = ballSpeed * cos(redBall.theta)
@@ -58,21 +76,36 @@ function initValue() {
     stroke(255, 100)
 }
 
+// setup関数
 function setup() {
     fullScreen()
-    buttonCreation()
-    buttonSettings()
+    elCreate()
+    elInit()
     initValue()
 }
 
+// draw関数
 function draw() {
     background(0)
     redBall.move()
     blueBall.move()
     yellowBall.move()
     time++
+    console.log(frameRate())
+    if (mouseIsPressed && mouseButton == RIGHT) {
+        elementX = mouseX
+        elementY = mouseY + windowHeight / 10
+        parentDiv.position(elementX, elementY)
+    }
+    for(let i = 0;i < blueBall.arrx.length;i++){
+        line(0, blueBall.arry[i] + ballRadi, width, blueBall.arry[i] + ballRadi)
+    }
+    for(let i = 0;i < yellowBall.arrx.length;i++){
+        line(yellowBall.arrx[i] + ballRadi, 0, yellowBall.arrx[i] + ballRadi, height)
+    }
 }
 
+// Ballクラス
 class Ball {
     constructor(x, y, sx0, sy0, t, i) {
         this.posx = x
@@ -92,24 +125,26 @@ class Ball {
         } else {
             this.speedy = this.speedy0 - gravity * time / 60
         }
-        this.posx += this.speedx0
-        this.posy -= this.speedy
-        image(this.img, this.posx, this.posy)
-        if (time % 10 == 0 && time > 0) {
-            this.arrx.push(this.posx)
-            this.arry.push(this.posy)
+        if (-2*width < this.posx && this.posx < 2*width && -2*height < this.posy && this.posy < 2*height){
+            this.posx += this.speedx0
+            this.posy -= this.speedy
+            image(this.img, this.posx, this.posy)
+            if (time % 10 == 0 && time > 0) {
+                this.arrx.push(this.posx)
+                this.arry.push(this.posy)
+            }
         }
+        
         for (let i = 0; i < this.arrx.length; i++) {
             tint(255, 177)
             image(this.img, this.arrx[i], this.arry[i])
-            line(0, this.arry[i] + ballRadi, width, this.arry[i] + ballRadi)
-            line(this.arrx[i] + ballRadi, 0, this.arrx[i] + ballRadi, height)
         }
     }
 }
 
+// リサイズされた時の処理
 function windowResized() {
     fullScreen()
-    buttonSettings()
+    elInit()
     initValue()
 }
