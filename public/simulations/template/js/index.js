bb14e49a3c5ec2bf312f9b626c1a1693c3974460
ohiaeni <jitianxianer018@gmail.com>
2023-06-12 16:55:26 +0900

    テンプレートの作成

---
+++ public/simulations/template/js/index.js
@@ -0,0 +1,34 @@
+function fullScreen() {
+    createCanvas(windowWidth, 9 * windowHeight / 10)
+}
+
+function elCreate() {
+
+}
+
+function elInit() {
+
+}
+
+function initValue() {
+
+}
+
+function setup() {
+    fullScreen()
+    elCreate()
+    elInit()
+}
+
+function draw() {
+    background(100)
+}
+
+function windowResized() {
+    fullScreen()
+    elInit()
+}
+
+// class Sample{
+
+// }
