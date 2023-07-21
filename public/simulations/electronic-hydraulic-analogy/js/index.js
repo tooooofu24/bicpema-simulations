e62fcf3262eb4dead945cc0ee46577e477e2c856
ohiaeni <jitianxianer018@gmail.com>
2023-07-21 15:06:33 +0900

    水流モデルの更新

--- public/simulations/electronic-hydraulic-analogy/js/index.js
+++ public/simulations/electronic-hydraulic-analogy/js/index.js
@@ -18,8 +18,10 @@ function elInit() {
 
 }
 
+let voltageValue;
 // 初期値やシミュレーションの設定
 function initValue() {
+    voltageValue = 200
 }
 
 // setup関数
@@ -28,41 +30,196 @@ function setup() {
     elCreate()
     elInit()
     initValue()
+    camera(300, -300, 300, 0, 0, 0, 0, 1, 0)
+}
+
+function drawFoundation() {
+    beginShape()
+    vertex(0, 0, 0)
+    vertex(150, 0, 0)
+    vertex(150, 0, 50)
+    vertex(0, 0, 50)
+    endShape()
+
+    beginShape()
+    vertex(0, 0, 0)
+    vertex(150, 0, 0)
+    vertex(150, -25, 0)
+    vertex(0, -25, 0)
+    endShape()
+
+    beginShape()
+    vertex(100, 0, 0)
+    vertex(150, 0, 0)
+    vertex(150, 0, 150)
+    vertex(100, 0, 150)
+    endShape()
+    
+    beginShape()
+    vertex(150, 0, 0)
+    vertex(150, 0, 150)
+    vertex(150, -25, 150)
+    vertex(150, -25, 0)
+    endShape()
+
+    beginShape()
+    vertex(0, 0, 0)
+    vertex(50, 0, 0)
+    vertex(50, 0, 100)
+    vertex(0, 0, 100)
+    endShape()
+
+    beginShape()
+    vertex(150, 0, 150)
+    vertex(100, 0, 150)
+    vertex(100, -25, 150)
+    vertex(150, -25, 150)
+    endShape()
+
+    beginShape()
+    vertex(150, 0, 150)
+    vertex(100, 0, 150)
+    vertex(100, -25, 150)
+    vertex(150, -25, 150)
+    endShape()
+
+    beginShape()
+    vertex(100, 0, 150)
+    vertex(100, 0, 50)
+    vertex(100, -25, 50)
+    vertex(100, -25, 150)
+    endShape()
+
+    beginShape()
+    vertex(100, 0, 50)
+    vertex(50, 0, 50)
+    vertex(50, -25, 50)
+    vertex(100, -25, 50)
+    endShape()
+
+    beginShape()
+    vertex(0, 0, 0)
+    vertex(0, 0, 100)
+    vertex(0, -25, 100)
+    vertex(0, -25, 0)
+    endShape()
+
+    beginShape()
+    vertex(50, 0, 50)
+    vertex(50, 0, 100)
+    vertex(50, -25, 100)
+    vertex(50, -25, 50)
+    endShape()
+
+    beginShape()
+    vertex(0, 0, 100)
+    vertex(0, voltageValue, 200)
+    vertex(50, voltageValue, 200)
+    vertex(50, 0, 100)
+    endShape()
+
+    beginShape()
+    vertex(50, 0, 100)
+    vertex(50, voltageValue, 200)
+    vertex(50, voltageValue-25, 200)
+    vertex(50, -25, 100)
+    endShape()
+
+    beginShape()
+    vertex(0, 0, 100)
+    vertex(0, voltageValue, 200)
+    vertex(0, voltageValue-25, 200)
+    vertex(0, -25, 100)
+    endShape()
+
+    beginShape()
+    vertex(0, voltageValue, 200)
+    vertex(0, voltageValue, 300)
+    vertex(50, voltageValue, 300)
+    vertex(50, voltageValue, 200)
+    endShape()
+
+    beginShape()
+    vertex(50, voltageValue, 200)
+    vertex(50, voltageValue, 250)
+    vertex(50, voltageValue-25, 250)
+    vertex(50, voltageValue-25, 200)
+    endShape()
+
+    beginShape()
+    vertex(50, voltageValue, 250)
+    vertex(100, voltageValue, 250)
+    vertex(100, voltageValue-25, 250)
+    vertex(50, voltageValue-25, 250)
+    endShape()
+
+    beginShape()
+    vertex(100, voltageValue, 250)
+    vertex(100, voltageValue, 100)
+    vertex(100, voltageValue-25, 100)
+    vertex(100, voltageValue-25, 250)
+    endShape()
+
+    beginShape()
+    vertex(100, voltageValue, 100)
+    vertex(150, voltageValue, 100)
+    vertex(150, voltageValue-25, 100)
+    vertex(100, voltageValue-25, 100)
+    endShape()
+
+    beginShape()
+    vertex(150, voltageValue, 300)
+    vertex(150, voltageValue, 100)
+    vertex(150, voltageValue-25, 100)
+    vertex(150, voltageValue-25, 300)
+    endShape()
+
+    beginShape()
+    vertex(0, voltageValue, 300)
+    vertex(150, voltageValue, 300)
+    vertex(150, voltageValue-25, 300)
+    vertex(0, voltageValue-25, 300)
+    endShape()
+
+    beginShape()
+    vertex(0, voltageValue, 300)
+    vertex(0, voltageValue, 200)
+    vertex(0, voltageValue-25, 200)
+    vertex(0, voltageValue-25, 300)
+    endShape()
+
+    beginShape()
+    vertex(50, voltageValue, 300)
+    vertex(150, voltageValue, 300)
+    vertex(150, voltageValue, 250)
+    vertex(50, voltageValue, 250)
+    endShape()
+
+    beginShape()
+    vertex(100, voltageValue, 250)
+    vertex(100, voltageValue, 100)
+    vertex(150, voltageValue, 100)
+    vertex(150, voltageValue, 250)
+    endShape()
+
+    // ポンプと電源を繋いでいる部分
+    push()
+    translate(125, voltageValue/2, 125)
+    cylinder(5, voltageValue, 10, 10, true, true)
+    pop()
+
+    // 電源
+    push()
+    translate(125, voltageValue/2, 125)
+    box(50)
+    pop()
 }
 
 // draw関数
 function draw() {
     orbitControl(10)
     background(100)
-    strokeWeight(10)
-    stroke(255, 0, 0)
-    line(0, 0, 0, 1000, 0, 0)
-    stroke(0, 255, 0)
-    line(0, 0, 0, 0, 1000, 0)
-    stroke(0, 0, 255)
-    line(0, 0, 0, 0, 0, 1000)
-    strokeWeight(1)
-
-    beginShape()
-    vertex(0,0,0)
-    vertex(200,0,0)
-    vertex(200,0,75)
-    vertex(0,0,75)
-    endShape()
-
-    beginShape()
-    vertex(125,0,0)
-    vertex(200,0,0)
-    vertex(200,0,200)
-    vertex(125,0,200)
-    endShape()
-
-    beginShape()
-    vertex(0,0,0)
-    vertex(75,0,0)
-    vertex(75,0,100)
-    vertex(0,0,100)
-    endShape()
+    drawFoundation()
 }
 
 // windowがリサイズされたときの処理
