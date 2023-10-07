bbec68ff95cab404fd6dd405fb5fc5aecd7d91f5
吉田賢二 <jitianxianer018@gmail.com>
2023-10-07 17:52:00 +0900

    3D地層観察の更新

--- public/simulations/3d-strata/js/index.js
+++ public/simulations/3d-strata/js/index.js
@@ -5,7 +5,9 @@ function fullScreen() {
 
 // 外部ファイルの読み込み
 function preload() {
-    dataTable = loadTable("./data/sample-data.csv","csv","header")
+    dataTable = loadTable("./data/sample-data.csv", "csv", "header")
+    engFont = loadFont('js/OPTITimes-Roman.otf');
+    jaFont = loadFont('js/ZenMaruGothic-Regular.ttf');
 }
 
 // DOM要素の生成
@@ -34,6 +36,10 @@ function initValue() {
     shallowArr = dataTable.getColumn("shallow-point")
     deepArr = dataTable.getColumn("deep-point")
     kindsArr = dataTable.getColumn("kinds")
+    textSize(25)
+    textFont(jaFont);
+    textAlign(CENTER);
+
 }
 
 // setup関数
@@ -59,34 +65,60 @@ function backgroundSetting() {
     // 格子線
     strokeWeight(1)
     stroke(0)
-    for (let x = 50; x <= 500; x += 50) {
+    fill(0)
+    for (let x = 0; x <= 500; x += 50) {
         line(x, 0, 0, x, 500, 0)
         line(x, 0, 0, x, 0, 500)
+        if (x % 100 == 0) {
+            push()
+            translate(0, 0, 500)
+            text(x, x, -10, 0)
+            pop()
+        }
     }
-    for (let y = 50; y <= 500; y += 50) {
+    push()
+    translate(0, 0, 500)
+    text("緯度", 250, -50)
+    pop()
+
+    for (let y = 0; y <= 500; y += 50) {
         line(0, y, 0, 500, y, 0)
         line(0, y, 0, 0, y, 500)
+        if (y % 100 == 0) text(y, 0, y, 0)
     }
-    for (let z = 50; z <= 500; z += 50) {
+    text("深さ", -50, 250, 0)
+    for (let z = 0; z <= 500; z += 50) {
         line(0, 0, z, 500, 0, z)
         line(0, 0, z, 0, 500, z)
+        if (z % 100 == 0) {
+            push()
+            rotateY(PI / 2)
+            translate(-z, 0, 500)
+            text(z, 0, -10)
+            pop()
+        }
     }
+    push()
+    rotateY(PI / 2)
+    translate(-250, -50, 500)
+    text("経度", 0, -10)
+    pop()
 }
 
-function createPlane1(x1,z1,y1,x2,z2,y2,x3,z3,y3){
+function createPlane1(x1, z1, y1, x2, z2, y2, x3, z3, y3) {
     beginShape()
-    vertex(x1,y1,z1)
-    vertex(x2,y2,z2)
-    vertex(x3,y3,z3)
+    vertex(x1, y1, z1)
+    vertex(x2, y2, z2)
+    vertex(x3, y3, z3)
     endShape(CLOSE)
 }
 
-function createPlane2(x1,z1,y1,x2,z2,y2,x3,z3,y3,x4,z4,y4){
+function createPlane2(x1, z1, y1, x2, z2, y2, x3, z3, y3, x4, z4, y4) {
     beginShape()
-    vertex(x1,y1,z1)
-    vertex(x2,y2,z2)
-    vertex(x3,y3,z3)
-    vertex(x4,y4,z4)
+    vertex(x1, y1, z1)
+    vertex(x2, y2, z2)
+    vertex(x3, y3, z3)
+    vertex(x4, y4, z4)
     endShape(CLOSE)
 }
 
@@ -94,34 +126,35 @@ function createPlane2(x1,z1,y1,x2,z2,y2,x3,z3,y3,x4,z4,y4){
 function draw() {
     orbitControl(2)
     backgroundSetting()
-    for(let i = 0; i < placeArr.length; i++){
+    for (let i = 0; i < placeArr.length; i++) {
         let x = xArr[i]
         let y = yArr[i]
         let z = shallowArr[i]
-        let zLength = deepArr[i]-shallowArr[i]
+        let zLength = deepArr[i] - shallowArr[i]
         let kind = kindsArr[i]
-        if(kind == "泥岩")fill(100,150)
-        if(kind == "砂岩")fill(116,80,48,150)
+        if (kind == "泥岩") fill(100, 150)
+        if (kind == "砂岩") fill(116, 80, 48, 150)
         push()
-        translate(x,int(z)+zLength/2,y)
-        box(50,zLength,50)
+        translate(x, int(z) + zLength / 2, y)
+        box(50, zLength, 50)
         pop()
     }
-    fill(100,50)
+    fill(100, 50)
     //泥岩のサンプル
-    createPlane1(xArr[0],yArr[0],shallowArr[0],xArr[2],yArr[2],shallowArr[2],xArr[4],yArr[4],shallowArr[4])
-    createPlane1(xArr[0],yArr[0],deepArr[0],xArr[2],yArr[2],deepArr[2],xArr[4],yArr[4],deepArr[4])
-    createPlane2(xArr[0],yArr[0],shallowArr[0],xArr[2],yArr[2],shallowArr[2],xArr[2],yArr[2],deepArr[2],xArr[0],yArr[0],deepArr[0])
-    createPlane2(xArr[0],yArr[0],shallowArr[0],xArr[4],yArr[4],shallowArr[4],xArr[4],yArr[4],deepArr[4],xArr[0],yArr[0],deepArr[0])
-    createPlane2(xArr[2],yArr[2],shallowArr[2],xArr[4],yArr[4],shallowArr[4],xArr[4],yArr[4],deepArr[4],xArr[2],yArr[2],deepArr[2])
+    createPlane1(xArr[0], yArr[0], shallowArr[0], xArr[2], yArr[2], shallowArr[2], xArr[4], yArr[4], shallowArr[4])
+    createPlane1(xArr[0], yArr[0], deepArr[0], xArr[2], yArr[2], deepArr[2], xArr[4], yArr[4], deepArr[4])
+    createPlane2(xArr[0], yArr[0], shallowArr[0], xArr[2], yArr[2], shallowArr[2], xArr[2], yArr[2], deepArr[2], xArr[0], yArr[0], deepArr[0])
+    createPlane2(xArr[0], yArr[0], shallowArr[0], xArr[4], yArr[4], shallowArr[4], xArr[4], yArr[4], deepArr[4], xArr[0], yArr[0], deepArr[0])
+    createPlane2(xArr[2], yArr[2], shallowArr[2], xArr[4], yArr[4], shallowArr[4], xArr[4], yArr[4], deepArr[4], xArr[2], yArr[2], deepArr[2])
 
-    fill(116,80,48,50)
+    fill(116, 80, 48, 50)
     //泥岩のサンプル
-    createPlane1(xArr[1],yArr[1],shallowArr[1],xArr[3],yArr[3],shallowArr[3],xArr[5],yArr[5],shallowArr[5])
-    createPlane1(xArr[1],yArr[1],deepArr[1],xArr[3],yArr[3],deepArr[3],xArr[5],yArr[5],deepArr[5])
-    createPlane2(xArr[1],yArr[1],shallowArr[1],xArr[3],yArr[3],shallowArr[3],xArr[3],yArr[3],deepArr[3],xArr[1],yArr[1],deepArr[1])
-    createPlane2(xArr[1],yArr[1],shallowArr[1],xArr[5],yArr[5],shallowArr[5],xArr[5],yArr[5],deepArr[5],xArr[1],yArr[1],deepArr[1])
-    createPlane2(xArr[3],yArr[3],shallowArr[3],xArr[5],yArr[5],shallowArr[5],xArr[5],yArr[5],deepArr[5],xArr[3],yArr[3],deepArr[3])
+    createPlane1(xArr[1], yArr[1], shallowArr[1], xArr[3], yArr[3], shallowArr[3], xArr[5], yArr[5], shallowArr[5])
+    createPlane1(xArr[1], yArr[1], deepArr[1], xArr[3], yArr[3], deepArr[3], xArr[5], yArr[5], deepArr[5])
+    createPlane2(xArr[1], yArr[1], shallowArr[1], xArr[3], yArr[3], shallowArr[3], xArr[3], yArr[3], deepArr[3], xArr[1], yArr[1], deepArr[1])
+    createPlane2(xArr[1], yArr[1], shallowArr[1], xArr[5], yArr[5], shallowArr[5], xArr[5], yArr[5], deepArr[5], xArr[1], yArr[1], deepArr[1])
+    createPlane2(xArr[3], yArr[3], shallowArr[3], xArr[5], yArr[5], shallowArr[5], xArr[5], yArr[5], deepArr[5], xArr[3], yArr[3], deepArr[3])
+
 }
 
 // windowがリサイズされたときの処理
