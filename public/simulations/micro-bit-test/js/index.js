1f30317aca3c153f22282aa7d7283f3370e0a68b
ohiaeni <jitianxianer018@gmail.com>
2023-10-18 18:50:43 +0900

    micro-bit-testの追加

---
+++ public/simulations/micro-bit-test/js/index.js
@@ -0,0 +1,29 @@
+// ///全画面表示
+// function fullScreen() {
+//     createCanvas(windowWidth, 9 * windowHeight / 10)
+// }
+
+// // DOM要素の生成
+// function elCreate() {
+// }
+// // 初期値やシミュレーションの設定
+// function initValue() {
+// }
+
+// // setup関数
+// function setup() {
+//     fullScreen()
+//     elCreate()
+//     initValue()
+// }
+
+// // draw関数
+// function draw() {
+//     background(255);
+// }
+
+// // windowがリサイズされたときの処理
+// function windowResized() {
+//     fullScreen()
+//     initValue()
+// }
