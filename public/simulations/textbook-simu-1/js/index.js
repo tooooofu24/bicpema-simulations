d59377a553ea4caf12d8d35d6bdf9f7c62eaa56a
吉田賢二 <jitianxianer018@gmail.com>
2024-02-07 14:29:23 +0900

    等速直線運動をする模型自動車のようすの更新

--- public/simulations/textbook-simu-1/js/index.js
+++ public/simulations/textbook-simu-1/js/index.js
@@ -1,11 +1,10 @@
 function fullScreen() {
-    createCanvas(2 * windowWidth / 3, 9 * windowHeight / 10)
+    createCanvas(2 * windowWidth / 3, 9 * windowHeight / 10);
 }
 
 function preload() {
-    yellowCarImg = loadImage("/assets/img/yellowCar.png")
-    redCarImg = loadImage("/assets/img/redCar.png")
-
+    yellowCarImg = loadImage("/assets/img/yellowCar.png");
+    redCarImg = loadImage("/assets/img/redCar.png");
 }
 
 function elCreate() {
@@ -16,20 +15,34 @@ function elInit() {
 
 let canvasWidth,
     canvasHeight;
-
 let yellowCar,
     redCar;
 let time;
+let scale;
+
 function initValue() {
     canvasWidth = 2 * windowWidth / 3;
     canvasHeight = 9 * windowHeight / 10;
-    yellowCarImg.resize(150, 0)
-    redCarImg.resize(150, 0)
+    yellowCarImg.resize(150, 0);
+    redCarImg.resize(150, 0);
     yellowCar = new CAR(0, canvasHeight / 2 - yellowCarImg.height - 50, yellowCarImg, 3, []);
-    redCar = new CAR(0, canvasHeight - redCarImg.height - 50, redCarImg, 4, []);
+    redCar = new CAR(0, canvasHeight - redCarImg.height - 50, redCarImg, 2, []);
     time = 0;
     textSize(15);
     textAlign(CENTER);
+    frameRate(30);
+    scale = createGraphics(canvasWidth, 50);
+    scale.background(255);
+    scale.textAlign(CENTER);
+    for (let x = 0; x < canvasWidth; x += 5) {
+        if (x % 50 == 0) {
+            scale.line(x, 0, x, 25);
+            scale.text(x / 50, x, 40);
+        }
+        else {
+            scale.line(x, 0, x, 15);
+        }
+    }
 }
 
 function setup() {
@@ -41,39 +54,22 @@ function setup() {
 
 function draw() {
     background(0);
-    drawScale();
     redCar._draw();
     yellowCar._draw();
-
-    if (time % 120 == 0) {
-        yellowCar.arr.push(yellowCar.posx)
-        redCar.arr.push(redCar.posx)
+    if (time % 30 == 0) {
+        yellowCar.arr.push(yellowCar.posx);
+        redCar.arr.push(redCar.posx);
     }
     time += 1;
+    image(scale, 0, canvasHeight / 2 - 50);
+    image(scale, 0, canvasHeight - 50);
+    console.log(frameRate())
 }
 
 function windowResized() {
     fullScreen();
     elInit();
     initValue();
-}
-
-function drawScale() {
-    fill(255);
-    rect(0, canvasHeight / 2 - 50, canvasWidth, 50)
-    rect(0, canvasHeight - 50, canvasWidth, 50)
-    fill(0)
-    for (let x = 0; x < canvasWidth; x += 5) {
-        if (x % 50 == 0) {
-            line(x, canvasHeight / 2 - 50, x, canvasHeight / 2 - 25);
-            line(x, canvasHeight - 50, x, canvasHeight - 25);
-            text(x / 50, x, canvasHeight / 2 - 10);
-            text(x / 50, x, canvasHeight - 10);
-        } else {
-            line(x, canvasHeight / 2 - 50, x, canvasHeight / 2 - 35);
-            line(x, canvasHeight - 50, x, canvasHeight - 35);
-        }
-    }
 }
 
 class CAR {
@@ -85,10 +81,12 @@ class CAR {
         this.arr = a;
     }
     _draw() {
-        this.posx += this.speed;
-        image(this.img, this.posx, this.posy);
+        tint(255, 150);
         for (let i = 0; i < this.arr.length; i++) {
-            image(this.img, this.arr[i], this.posy)
+            image(this.img, this.arr[i] - this.img.width / 2, this.posy);
         }
+        this.posx += 50 * this.speed / frameRate();
+        tint(255);
+        image(this.img, this.posx - this.img.width / 2, this.posy);
     }
 }
