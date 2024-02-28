c349918a7cd408a2ca19807ef660c279e195259f
吉田賢二 <jitianxianer018@gmail.com>
2024-02-28 16:59:04 +0900

    等速直線運動をする模型自動車のようすの更新

--- public/simulations/textbook-simu-1/js/index.js
+++ public/simulations/textbook-simu-1/js/index.js
@@ -20,6 +20,10 @@ let upperGraph,
     lowerGraph;
 let upperGraphButton,
     lowerGraphButton;
+let upperGraphButtonX_T,
+    upperGraphButtonV_T;
+let lowerGraphButtonX_T,
+    lowerGraphButtonV_T;
 function elCreate() {
     upperGraphParent = select("#upperGraphParent")
     lowerGraphParent = select("#lowerGraphParent")
@@ -27,12 +31,37 @@ function elCreate() {
     lowerGraph = select("#lowerGraph")
     upperGraphButton = select("#upperGraphButton")
     lowerGraphButton = select("#lowerGraphButton")
+    upperGraphButtonX_T = select("#upperGraphButtonX_T")
+    upperGraphButtonV_T = select("#upperGraphButtonV_T")
+    lowerGraphButtonX_T = select("#lowerGraphButtonX_T")
+    lowerGraphButtonV_T = select("#lowerGraphButtonV_T")
 }
 
+function upperGraphButtonX_TFunction() {
+    upperGraphData = true
+}
+
+function upperGraphButtonV_TFunction() {
+    upperGraphData = false
+}
+
+function lowerGraphButtonX_TFunction() {
+    lowerGraphData = true
+}
+
+function lowerGraphButtonV_TFunction() {
+    lowerGraphData = false
+}
+
+
 function elInit() {
-    lowerGraphParent.position(width, 55 * windowHeight / 100).size(windowWidth/3,4*windowHeight/10);
+    lowerGraphParent.position(width, 55 * windowHeight / 100).size(windowWidth / 3, 4 * windowHeight / 10);
     upperGraphButton.position(width, 5 * windowHeight / 10)
     lowerGraphButton.position(width, 9.5 * windowHeight / 10)
+    upperGraphButtonX_T.mousePressed(upperGraphButtonX_TFunction)
+    upperGraphButtonV_T.mousePressed(upperGraphButtonV_TFunction)
+    lowerGraphButtonX_T.mousePressed(lowerGraphButtonX_TFunction)
+    lowerGraphButtonV_T.mousePressed(lowerGraphButtonV_TFunction)
 }
 
 let canvasWidth,
@@ -40,33 +69,38 @@ let canvasWidth,
 let yellowCar,
     redCar;
 let time;
-let scale;
+let scaleImg;
 let countArray;
+let upperGraphData,
+    lowerGraphData;
 
 function initValue() {
     canvasWidth = 2 * windowWidth / 3;
     canvasHeight = 9 * windowHeight / 10;
     yellowCarImg.resize(150, 0);
     redCarImg.resize(150, 0);
-    yellowCar = new CAR(0, canvasHeight / 2 - yellowCarImg.height - 50, yellowCarImg, 3, []);
-    redCar = new CAR(0, canvasHeight - redCarImg.height - 50, redCarImg, 2, []);
+    yellowCar = new CAR(0, canvasHeight / 2 - yellowCarImg.height - 50, yellowCarImg, 3, [], []);
+    redCar = new CAR(0, canvasHeight - redCarImg.height - 50, redCarImg, 2, [], []);
     time = 0;
     textSize(15);
     textAlign(CENTER);
     frameRate(30);
-    scale = createGraphics(canvasWidth, 50);
-    scale.background(255);
-    scale.textAlign(CENTER);
+    scaleImg = createGraphics(canvasWidth, 50);
+    scaleImg.background(255);
+    scaleImg.textAlign(CENTER);
     for (let x = 0; x < canvasWidth; x += 5) {
         if (x % 50 == 0) {
-            scale.line(x, 0, x, 25);
-            scale.text(x / 50, x, 40);
+            scaleImg.line(x, 0, x, 25);
+            scaleImg.text(x / 50, x, 40);
         }
         else {
-            scale.line(x, 0, x, 15);
+            scaleImg.line(x, 0, x, 15);
         }
     }
     countArray = [];
+    // graphのデータはtrueのときにはX-T、falseの時にはV-T
+    upperGraphData = true
+    lowerGraphData = true
 }
 
 function setup() {
@@ -78,13 +112,17 @@ function setup() {
 
 function draw() {
     background(0);
-    image(scale, 0, canvasHeight / 2 - 50);
-    image(scale, 0, canvasHeight - 50);
+    image(scaleImg, 0, canvasHeight / 2 - 50);
+    image(scaleImg, 0, canvasHeight - 50);
     redCar._draw();
     yellowCar._draw();
-    if (time % 30 == 0) {
-        yellowCar.arr.push(yellowCar.posx);
-        redCar.arr.push(redCar.posx);
+
+    // time <= 300により１０秒間の計測を実装
+    if (time % 30 == 0 && time <= 300) {
+        yellowCar.xarr.push(yellowCar.posx);
+        redCar.xarr.push(redCar.posx);
+        yellowCar.varr.push(yellowCar.speed);
+        redCar.varr.push(redCar.speed);
         countArray.push(int(time / 30))
     }
     time += 1;
@@ -98,20 +136,21 @@ function windowResized() {
 }
 
 class CAR {
-    constructor(x, y, i, v, a) {
+    constructor(x, y, i, v, xa, va) {
         this.posx = x;
         this.posy = y;
         this.img = i;
         this.speed = v;
-        this.arr = a;
+        this.xarr = xa;
+        this.varr = va;
     }
     _draw() {
         tint(255, 150);
         stroke(255, 0, 0);
         strokeWeight(3)
-        for (let i = 0; i < this.arr.length; i++) {
-            image(this.img, this.arr[i] - this.img.width / 2 - this.arr[0], this.posy);
-            line(this.arr[i] - this.arr[0], this.posy + this.img.height - 10, this.arr[i] - this.arr[0], this.posy + this.img.height + 10);
+        for (let i = 0; i < this.xarr.length; i++) {
+            image(this.img, this.xarr[i] - this.img.width / 2 - this.xarr[0], this.posy);
+            line(this.xarr[i] - this.xarr[0], this.posy + this.img.height - 10, this.xarr[i] - this.xarr[0], this.posy + this.img.height + 10);
         }
         this.posx += 50 * this.speed / 30;
         tint(255);
@@ -129,6 +168,15 @@ function graphDraw() {
 
     // upperGraph
 
+    let upperData,
+        upperText;
+    if (upperGraphData) {
+        upperData = yellowCar.xarr
+        upperText = "黄色い車のx-tグラフ"
+    } else {
+        upperData = yellowCar.varr
+        upperText = "黄色い車のv-tグラフ"
+    }
     if (typeof graphChart1 !== 'undefined' && graphChart1) {
         graphChart1.destroy();
     }
@@ -136,8 +184,7 @@ function graphDraw() {
     let data1 = {
         labels: countArray,
         datasets: [{
-            label: 'Dataset1',
-            data: yellowCar.arr
+            data: upperData
         }
         ]
     }
@@ -145,8 +192,17 @@ function graphDraw() {
         plugins: {
             title: {
                 display: true,
-                text: 'v-tグラフ'
+                text: upperText
             },
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                min: 0,
+                max: 10
+            }
         },
         animation: false,
         maintainAspectRatio: false
@@ -159,6 +215,15 @@ function graphDraw() {
 
     // lowerGraph
 
+    let lowerData,
+        lowerText;
+    if (lowerGraphData) {
+        lowerData = redCar.xarr
+        lowerText = "赤い車のx-tグラフ"
+    } else {
+        lowerData = redCar.varr
+        lowerText = "赤い車のv-tグラフ"
+    }
     if (typeof graphChart3 !== 'undefined' && graphChart3) {
         graphChart3.destroy();
     }
@@ -166,8 +231,7 @@ function graphDraw() {
     let data3 = {
         labels: countArray,
         datasets: [{
-            label: 'Dataset3',
-            data: yellowCar.arr
+            data: lowerData
         }
         ]
     }
@@ -175,8 +239,17 @@ function graphDraw() {
         plugins: {
             title: {
                 display: true,
-                text: 'v-tグラフ'
+                text: lowerText
             },
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                min: 0,
+                max: 10
+            }
         },
         animation: false,
         maintainAspectRatio: false
