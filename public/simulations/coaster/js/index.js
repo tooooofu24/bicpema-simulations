cab20fb252ec77a8d7828d123b9bfbe80acb20db
吉田賢二 <jitianxianer018@gmail.com>
2024-06-05 21:36:52 +0900

    コースターの追加

---
+++ public/simulations/coaster/js/index.js
@@ -0,0 +1,34 @@
+// index.jsはメインのメソッドを呼び出すためのファイルです。
+
+/////////////////////////// 以上の記述は不必要であれば削除してください。/////////////////////////////////
+
+// setup関数
+// シミュレーションを実行する際に１度だけ呼び出される。
+function setup() {
+  settingInit();
+  elementSelectInit();
+  elementPositionInit();
+  valueInit();
+}
+
+// draw関数
+// シミュレーションを実行した後、繰り返し呼び出され続ける
+function draw() {
+  scale(width / 1000);
+  background(255);
+  // drawGraph();
+
+  setBackground(slopeHeight, diameter);
+  drawScale();
+  test();
+  ball.update(slopeHeight);
+  ball.draw();
+  deviceJudge.rotateInstruction();
+}
+
+// windowResized関数
+// シミュレーションを利用しているデバイスの画面サイズが変わった際に呼び出される。
+function windowResized() {
+  canvasController.resizeScreen();
+  elementPositionInit();
+}
