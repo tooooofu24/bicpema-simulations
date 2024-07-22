ade58379489e6fd7dcac6b59af0c30aeb5c1ce04
吉田賢二 <jitianxianer018@gmail.com>
2024-07-22 19:26:22 +0900

    ドップラー効果の更新

--- public/simulations/doppler/js/index.js
+++ public/simulations/doppler/js/index.js
@@ -1,151 +1,138 @@
-//音波のインスタンス
-let sounds;
-
-//ボタンのインスタンス
-let backgroundDiv, startButton, stopButton, resetButton, speedButton;
-
-//音源の座標
-let posx;
-let posy;
-
-//その他の変数
-let count;
-let resetCount;
-let clickedCount;
-let fps = 60;
-
-//フルスクリーンにする手続き
-function fullScreen() {
-  createCanvas(windowWidth, (8 * windowHeight) / 10);
+const W = 1000;
+const H = ((1000 * 9) / 16) * 0.9;
+let font = null;
+function preload() {
+  font = loadFont("../../assets/fonts/ZenMaruGothic-Regular.ttf");
 }
 
-//初期化する手続き
-function initSettings() {
-  frameRate(fps);
-  posx = 50;
-  posy = height / 2;
-  count = 0;
-  resetCount = true;
-  clickedCount = false;
-  panelCount = false;
-  textSize(width / 100);
+const FPS = 60;
+let deviceJudge = null,
+  canvasController = null;
+function settingInit() {
+  deviceJudge = new BicpemaDeviceJudge();
+  canvasController = new BicpemaCanvasController(true, false, 1.0, 0.9);
+  deviceJudge.judge();
+  canvasController.fullScreen();
+  frameRate(FPS);
   textAlign(CENTER, CENTER);
+  textFont(font);
+  textSize(16);
 }
 
-//ボタンを初期化するメソッド
-function buttonSettings() {
-  backgroundDiv.size(width, windowHeight / 10).style("background-color", "white");
-  startButton
-    .mousePressed(moveButtonAction)
-    .size(windowWidth / 3, windowHeight / 10)
-    .position(0, height + windowHeight / 10)
-    .addClass("btn btn-outline-primary fs-2")
-    .parent(backgroundDiv);
-  stopButton
-    .mousePressed(moveButtonAction)
-    .size(windowWidth / 3, windowHeight / 10)
-    .position(0, height + windowHeight / 10)
-    .addClass("btn btn-outline-danger fs-2")
-    .hide()
-    .parent(backgroundDiv);
-  resetButton
+let parentDiv = null,
+  startButton = null,
+  stopButton = null,
+  resetButton = null,
+  spanText = null;
+speedButton = null;
+function elementSelectInit() {
+  parentDiv = createDiv().addClass("input-group");
+  startButton = createButton("スタート")
+    .mousePressed(startButtonFunction)
+    .parent(parentDiv)
+    .addClass("btn btn-outline-primary");
+  stopButton = createButton("ストップ")
+    .mousePressed(stopButtonFunction)
+    .parent(parentDiv)
+    .addClass("btn btn-outline-danger");
+  resetButton = createButton("リセット")
     .mousePressed(resetButtonAction)
-    .size(windowWidth / 3, windowHeight / 10)
-    .position(windowWidth / 3, height + windowHeight / 10)
-    .addClass("btn btn-outline-secondary fs-2")
-    .parent(backgroundDiv);
-  speedButton
-    .size(windowWidth / 3, windowHeight / 10)
-    .position((2 * windowWidth) / 3, height + windowHeight / 10)
-    .style("text-align", "center")
-    .addClass("fs-2")
-    .parent(backgroundDiv);
+    .parent(parentDiv)
+    .addClass("btn btn-outline-secondary");
+  spanText = createSpan("音源の速度（m/s）").parent(parentDiv).addClass("input-group-text");
+
+  speedButton = createInput(340, "number").parent(parentDiv).addClass("form-control");
 }
 
-//スタートストップボタンの手続き
-function moveButtonAction() {
-  if (clickedCount == false) {
-    clickedCount = true;
-    resetCount = false;
-    startButton.hide();
-    stopButton.show();
-  } else {
-    clickedCount = false;
-    startButton.show();
-    stopButton.hide();
-  }
-}
-
-//リセットボタンの手続き
-function resetButtonAction() {
+let posx, posy;
+let count;
+let sounds;
+let clickedCount;
+function valueInit() {
   posx = 50;
-  resetCount = true;
-  clickedCount = false;
-  startButton.show();
-  stopButton.hide();
+  posy = H / 2;
   count = 0;
-  for (let i = 0; i < sounds.length; i++) {
-    sounds.length = 0;
-  }
+  sounds = [];
+  clickedCount = false;
 }
 
-//背景を描画する手続き
-function backGround() {
-  background(255);
-  stroke(0, 100);
-  for (let i = 0; i < width - 50; i += 10) {
-    if (i % 100 == 0) {
-      strokeWeight(2);
-    } else {
-      strokeWeight(1);
-    }
-    line(i + 50, 0, i + 50, height);
-  }
-  for (let i = 0; i < height / 2; i += 10) {
-    if (i % 100 == 0) {
-      strokeWeight(2);
-    } else {
-      strokeWeight(1);
-    }
-    line(50, height / 2 + i, width, height / 2 + i);
-    line(50, height / 2 - i, width, height / 2 - i);
-  }
-  stroke(0);
-}
-
-//setup関数
+// setup関数
+// シミュレーションを実行する際に１度だけ呼び出される。
 function setup() {
-  fullScreen();
-  initSettings();
-  sounds = new Array();
-  backgroundDiv = createElement("div");
-  startButton = createButton("スタート");
-  stopButton = createButton("ストップ");
-  resetButton = createButton("リセット");
-  speedButton = createInput(340, "number");
-  buttonSettings();
+  settingInit();
+  elementSelectInit();
+  valueInit();
 }
 
-//draw関数
+// draw関数
+// シミュレーションを実行した後、繰り返し呼び出され続ける
 function draw() {
+  scale(width / 1000);
+  background(255);
   backGround();
   if (clickedCount == true) {
     count++;
   }
-  if (count % (fps / 10) == 0 && clickedCount == true) {
+  if (count % (FPS / 10) == 0 && clickedCount == true) {
     sounds.push(new SOUND(posx, 0));
   }
   for (let i = 0; i < sounds.length; i++) {
     sounds[i]._draw();
   }
-  posx = (speedButton.value() * count) / fps + 50;
+  posx = (speedButton.value() * count) / FPS + 50;
   fill(0);
   ellipse(posx, posy, 20, 20);
   textSize(20);
   text(speedButton.value() + " m/s", posx, posy + 20);
+  deviceJudge.rotateInstruction("horizontal");
 }
 
-//音波オブジェクト
+// windowResized関数
+// シミュレーションを利用しているデバイスの画面サイズが変わった際に呼び出される。
+function windowResized() {
+  canvasController.resizeScreen();
+  valueInit();
+}
+
+//背景を描画する手続き
+function backGround() {
+  stroke(0, 100);
+  for (let i = 0; i < W - 50; i += 10) {
+    if (i % 100 == 0) {
+      strokeWeight(2);
+    } else {
+      strokeWeight(1);
+    }
+    line(i + 50, 0, i + 50, H);
+  }
+  for (let i = 0; i < H / 2; i += 10) {
+    if (i % 100 == 0) {
+      strokeWeight(2);
+    } else {
+      strokeWeight(1);
+    }
+    line(50, H / 2 + i, W, H / 2 + i);
+    line(50, H / 2 - i, W, H / 2 - i);
+  }
+  stroke(0);
+}
+
+function startButtonFunction() {
+  clickedCount = true;
+}
+
+function stopButtonFunction() {
+  clickedCount = false;
+}
+
+//リセットボタンの手続き
+function resetButtonAction() {
+  posx = 50;
+  clickedCount = false;
+  count = 0;
+  sounds = [];
+}
+
 class SOUND {
   constructor(x, r) {
     this.soundx = x;
@@ -153,17 +140,9 @@ class SOUND {
   }
   _draw() {
     if (clickedCount == true) {
-      this.radi += 340 / 60;
+      this.radi += 340 / FPS;
     }
     noFill();
-    ellipse(this.soundx, height / 2, this.radi * 2, this.radi * 2);
+    ellipse(this.soundx, H / 2, this.radi * 2, this.radi * 2);
   }
-}
-
-//ウィンドウがリサイズされた時の手続き
-function windowResized() {
-  fullScreen();
-  initSettings();
-  buttonSettings();
-  resetButtonAction();
 }
