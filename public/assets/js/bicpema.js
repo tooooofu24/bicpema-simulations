d1f368c023b7ed315ee20a841de80b4a8e29dc3f
吉田賢二 <jitianxianer018@gmail.com>
2024-08-11 16:16:28 +0900

    bicpema.jsにJSDocを導入

--- public/assets/js/bicpema.js
+++ public/assets/js/bicpema.js
@@ -1,13 +1,25 @@
-// BicpemaCanvasControllerクラス
-// Bicpemaの動的なキャンバスサイズをコントロールするオブジェクト
-// インスタンス名 = new BicpemaCanvasController();
+/**
+ * BicpemaCanvasControllerクラス
+ *
+ * Bicpemaの動的なキャンバスサイズをコントロールする。
+ */
 class BicpemaCanvasController {
+  /**
+   * @constructor
+   * @param {boolean} f 回転時に比率を固定化するか
+   * @param {boolean} i 3Dかどうか
+   * @param {number} w_r 幅の比率（0.0~1.0）
+   * @param {number} h_r 高さの比率（0.0~1.0）
+   */
   constructor(f = true, i = false, w_r = 1.0, h_r = 1.0) {
     this.fixed = f;
     this.is3D = i;
     this.widthRatio = w_r;
     this.heightRatio = h_r;
   }
+  /**
+   * HTML要素で生成している#p5Canvasと#navBarを元にcanvasを生成する。
+   */
   fullScreen() {
     const P5_CANVAS = select("#p5Canvas");
     const NAV_BAR = select("#navBar");
@@ -31,6 +43,10 @@ class BicpemaCanvasController {
     }
     canvas.parent(P5_CANVAS).class("rounded border border-1");
   }
+
+  /**
+   * HTML要素で生成している#p5Canvasと#navBarを元にcanvasをリサイズする。
+   */
   resizeScreen() {
     const NAV_BAR = select("#navBar");
     let w = 0;
@@ -51,15 +67,21 @@ class BicpemaCanvasController {
   }
 }
 
-// BicpemaDeviceJudgeクラス
-// Bicpemaを利用しているデバイスを判定するオブジェクト
-// インスタンス名 = new BicpemaDeviceJudge();
+/**
+ * Bicpemaを使用しているデバイスを判定し、デバイスに応じて画面表示を切り替える。
+ */
 class BicpemaDeviceJudge {
+  /**
+   * @constructor
+   */
   constructor() {
     this.r = 0;
     this.smartPhone;
     this.deviceIs = false;
   }
+  /**
+   * Bicpemaを使用しているデバイスを判定する。
+   */
   judge() {
     const U_A = navigator.userAgent;
     if (
@@ -90,6 +112,12 @@ class BicpemaDeviceJudge {
       this.deviceIs = false;
     }
   }
+
+  /**
+   * デバイスの回転を促す画像を表示する。
+   *
+   * @param {string} [direction="vertival"]
+   */
   rotateInstruction(direction = "vertical") {
     function smartPhoneDisplay(r, img) {
       let m = 1.4 * map(r, 0, 90, 0, 1);
@@ -122,56 +150,25 @@ class BicpemaDeviceJudge {
   }
 }
 
-// BicpemaCarクラス
-// インスタンス名 = new BicpemaCar(x座標, y座標, x方向の速度, y方向の速度, 車の色, 車の幅, x方向の加速度, y方向の加速度);
-// デフォルトの車の色はred("r")であり、他にはyellow("y")が用意されている。
-// 車の原則として高さとの相対値で決定する。
-class BicpemaCar {
-  constructor(x = 0, y = 0, vx = 0, vy = 0, k = "r", w = 100, ax = 0, ay = 0) {
-    this.posX = x;
-    this.posY = y;
-    this.speedX = vx;
-    this.speedY = vy;
-    this.kind = k;
-    this.acceX = ax;
-    this.acceY = ay;
-    this.carWidth = w;
-    this.carHeight;
-    this.rCar = loadImage("/assets/img/rCar.png");
-    this.yCar = loadImage("/assets/img/yCar.png");
-  }
-  carDraw() {
-    this.speedX += this.acceX / frameRate();
-    this.speedY += this.acceY / frameRate();
-    this.posX += this.speedX / frameRate();
-    this.posY += this.speedY / frameRate();
-    let img = "";
-    switch (this.kind) {
-      case "r":
-        img = this.rCar;
-        this.carHeight = this.carWidth * (this.rCar.height / this.rCar.width);
-        break;
-      case "y":
-        img = this.yCar;
-        this.carHeight = this.carWidth * (this.yCar.height / this.yCar.width);
-        break;
-      default:
-        img = this.rCar;
-        this.carHeight = this.carWidth * (this.rCar.height / this.rCar.width);
-    }
-    image(img, this.posX - this.carWidth / 2, this.posY - this.carHeight / 2, this.carWidth, this.carHeight);
-  }
-}
-
-// 3Dの座標系クラス
+/**
+ * 3D座標系を描画するクラス
+ */
 class CoordinateSystem {
+  /**
+   * @constructor
+   * @param {number} x x方向の長さ
+   * @param {number} y y方向の長さ
+   * @param {number} z z方向の長さ
+   */
   constructor(x = 500, y = 500, z = 500) {
     this.x = x;
     this.y = y;
     this.z = z;
   }
 
-  // x,y,z軸を描画するメソッド
+  /**
+   * x,y,z軸を描画する。
+   */
   line() {
     stroke(255, 0, 0);
     line(this.x, 0, 0, 0, 0, 0);
@@ -181,7 +178,9 @@ class CoordinateSystem {
     line(0, 0, this.z, 0, 0, 0);
   }
 
-  // x,y,z方向のスケールを描画するメソッド
+  /**
+   * x,y,z方向のスケールを描画するメソッド
+   */
   scale() {
     stroke(100, 100);
     for (let x = 50; x <= this.x; x += 50) {
@@ -198,7 +197,13 @@ class CoordinateSystem {
     }
   }
 
-  // x,y,z方向の軸ラベルを描画するメソッド
+  /**
+   * x,y,z方向の軸ラベルを描画するメソッド
+   * @param {string} xLabel x方向のラベル
+   * @param {string} yLabel y方向のラベル
+   * @param {string} zLabel z方向のラベル
+   * @param {number} size フォントサイズ
+   */
   axisLabel(xLabel, yLabel, zLabel, size) {
     fill(0);
     textSize(size);
